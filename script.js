6619dc7b8fcc6da824365cbb9326ada90412705f
Paulius <garbasauskas@gmail.com>
2018-03-25 15:37:09 +0300

    žaidėjas gali paspausti tik vieną kartą, kompiuteris pildo tik tuščius langelius, kas pradeda žaidimą išrenkama atsitiktinai

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 let playerTurn;
 let playerSymbol;
+let computerSymbol;
+let disabled;
 
 let game = {
     
@@ -34,66 +36,114 @@ let game = {
             this.dom(".buttons").style.display = "none";
             this.dom(".whose-turn").style.display = "block";
 
-            //Žaidėjo eilė
+            //Žaidėjas pasirinko būti "X"
             if (event.target.getAttributeNode("class").value === "x") {
-                console.log("paspaustas x");
-                playerTurn = true;
-                playerSymbol = "x";
+                console.log("Žaidėjas pasirinko būti X");
+                playerSymbol = "X";
+                computerSymbol = "0";
                 //Žaidėjo stilius ir jo eilės užrašas
-                this.whoseTurn();
-            //Kompiuterio eilė
+                this.whoStarts();
+            //Žaidėjas pasirinko būti "0"
             } else if (event.target.getAttributeNode("class").value === "o") {
-                console.log("paspaustas 0");
-                playerTurn = false;
+                console.log("Žaidėjas pasirinko būti 0");
                 playerSymbol = "0";
+                computerSymbol = "X";
                 //Kompiuterio stilius ir jo eilės užrašas
-                this.whoseTurn();
-                
-                //Po 1 sekundės uždelsimo
-                setTimeout(() => { 
-                    //Kompiuteris atsitiktinai renkasi kur įrašyti savo pirmą "0", nes jis pradeda ir visi kvadratai dar tušti
-                    let squares = [];
-                    for (let i = 0; i < this.dom(".frame").children.length; i++) {
-                        squares.push(this.dom(".frame").children[i]);
-                    }
-                    let randomNumber = parseInt(Math.random() * 9);
-                    squares[randomNumber].innerText = "0";
-                }, 1000);
-                //Dabar turi būti žmogaus eilė
-                setTimeout(this.whoseTurn, 1500);
+                this.whoStarts();
             }
         });
     },
+
+    //Atsitiktiniu būdu parenkama kas pradeda žaidimą 
+    whoStarts: function() {
+        let players = ["player", "computer"];
+        let randomNumber = parseInt(Math.random() * 2);
+        let whoStarts = players[randomNumber];
+        //Jeigu pradeda žmogus
+        if (whoStarts === "player") {
+            playerTurn = true;
+            this.whoseTurn(); //pritaikomas žmogaus stilius ir užrašas
+        //Jeigu pradeda kompiuteris
+        } else {
+            playerTurn = false;
+            //Po 1 sekundės uždelsimo
+            setTimeout(() => { 
+                //Kompiuteris atsitiktinai renkasi kur įrašyti savo pirmą simbolį, nes jis pradeda ir visi kvadratai dar tušti
+                let squares = [];
+                for (let i = 0; i < this.dom(".frame").children.length; i++) {
+                    squares.push(this.dom(".frame").children[i]);
+                }
+                let randomNumber = parseInt(Math.random() * 9);
+                squares[randomNumber].innerText = computerSymbol;
+                playerTurn = true;
+            }, 1000);
+            //Dabar žmogaus eilė
+            setTimeout(this.whoseTurn, 1500); //po 1,5 sekundės uždelsimo pritaikomas žmogaus stilius ir užrašas
+        }
+    },
     
-    //
+    //Parašo kieno eilė ir pagal tai pakeičia stilių 
     whoseTurn: function() {
+        //Žmogaus stilius ir jo paspaudimo nustatymai
         if (playerTurn) {
             game.dom(".whose-turn__who").innerText = "Your turn!";
             game.dom(".whose-turn__who").style.backgroundColor = "rgb(0,128,0)";
-            //Žaidėjas gali rinktis kur spausti savo ženklą
-            game.dom(".frame").addEventListener("click", (event) => {
-                //Žaidėjas gali spausti tik ant tuščio langelio
-                if (event.target.innerText === "") {
-                    //Paspaudimas veikia tik ant ".frame" vaiko elemento
-                    if (event.target !== event.currentTarget) {
-                        console.log("žaidėjas paspaudė");
-                        event.target.innerText = "X";
+            //Žmogus gali rinktis kur spausti savo ženklą
+                game.dom(".frame").addEventListener("click", (event) => {
+                    //Žmogus gali paspausti tik vieną kartą, paskui renkasi kompiuteris
+                    if (!disabled) {
+                        //Žmogus gali spausti tik ant tuščio langelio
+                        if (event.target.innerText === "") {
+                            //Paspaudimas veikia tik ant ".frame" vaiko elemento
+                            if (event.target !== event.currentTarget) {
+                                console.log("žaidėjas paspaudė");
+                                event.target.innerText = playerSymbol;
+                                disabled = true;
+                            }
+                            event.stopPropagation();
+                            playerTurn = false;
+                            game.computerPlays(); //Kompiuterio eilė
+                        } 
                     }
-                    event.stopPropagation();
-                }
-            });
-            playerTurn = false;
-        } else if (!playerTurn) {
+                });
+        //Kompiuterio stilius
+        } else {
             game.dom(".whose-turn__who").innerText = "Computer's turn!";
             game.dom(".whose-turn__who").style.backgroundColor = "rgb(0, 149, 240)";
             playerTurn = true;
         }
+    },
+
+    //Žaidžia kompiuteris
+    computerPlays: function() {
+        setTimeout(this.whoseTurn, 1000); //Po 1 sekundės atsiranda kompiuterio stilius
+        setTimeout(() => { 
+            //Po 1,5 sekundės kompiuteris atsitiktinai renkasi ir įrašo į kažkurį tuščią langelį savo simbolį
+            let squares = [];
+            for (let i = 0; i < this.dom(".frame").children.length; i++) {
+                if (this.dom(".frame").children[i].innerText === "") {
+                    squares.push(this.dom(".frame").children[i]);
+                }
+            }
+            if (squares.length !== 0) {
+                let randomNumber = parseInt(Math.random() * squares.length);
+                squares[randomNumber].innerText = computerSymbol;
+            } else {
+                console.log("End of the game!");
+            }
+            playerTurn = true;
+            disabled = false;
+        }, 1500);
+        //Po 1,5 sekundžių atsiranda žmogaus stilius ir jis gali paspausti ant langelio
+        setTimeout(this.whoseTurn, 1500);
     }
 };
 
 game.init();
 game.start();
 game.whoseTurn();
+
+
 
 
 
